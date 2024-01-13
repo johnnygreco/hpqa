b418f6c21d164defb6cca6bbe687509a56a9ad53
Johnny Greco <jgreco.ai@gmail.com>
2024-01-13 12:30:36 -0500

    dependencies for spaces

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import find_packages, setup
 
 install_requires = [
     "langchain==0.1.0",
+    "langchain-openai==0.0.2.post1",
+    "langchain-community==0.0.12",
+    "langchain-core==0.1.10",
     "gradio==4.14.0",
     "faiss-cpu==1.7.4",
     "openai==1.7.2",
